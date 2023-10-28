87736508755312f2ecc623a58ccbf4a113fa10ae
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-10-28 11:57:43 +0500

    tugadi

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,7 @@
   <div>
     <div class="tekshiruv">
       <div class="container">
-        <swiper
-        :modules="[SwiperAutoplay, SwiperEffectCreative, SwiperNavigation]"
-          :slidesPerView="3"
-          :grid="{
-            rows: 2,
-          }"
-          :spaceBetween="30"
-          :pagination="{
-            clickable: true,
-          }"
-          class="mySwiper"
-        >
-          <swiper-slide>Slide 1</swiper-slide>
-          <swiper-slide>Slide 2</swiper-slide
-          ><swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 4</swiper-slide
-          ><swiper-slide>Slide 5</swiper-slide>
-          <swiper-slide>Slide 6</swiper-slide
-          ><swiper-slide>Slide 7</swiper-slide>
-          <swiper-slide>Slide 8</swiper-slide
-          ><swiper-slide>Slide 9</swiper-slide>
-        </swiper>
+    
       </div>
     </div>
     <!-- <div class="hero">
